7ac68bbd79f3764d80e044478cc8bcd616e2a0a9
Евгений <tea-expansion@yandex.ru>
2023-07-02 17:44:12 +0300

    03

--- simple_signup/simple_signup/test_accounts.py
+++ simple_signup/simple_signup/test_accounts.py
@@ -1,3 +1,4 @@
 # basic_user             jghfjndydtnydtgff      basic_user@example.com
 # premium_user           gdhnytmmfimjjlkddd     premium_user@example.com
-# basic_test_user        mccltysvslghDdD         basic_test_user@example.com
+# basic_test_user        mccltysvslghDdD        basic_test_user@example.com
+# basic_test_user1       mu9o9i9lkcsfsfe        basic_test_user1@example.com
